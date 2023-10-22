bb3e3575f9ecc8690dd1941a6fd4b88ddfce9b8f
JMauricio6 <jmauriciorivera6@gmail.com>
2023-10-22 15:46:56 -0600

    Begin countries API Controller

---
+++ Sales/Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/Countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public CountriesController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _dataContext.Countries.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _dataContext.Add(country);
+            await _dataContext.SaveChangesAsync();
+            return Ok(country);
+        }
+
+    }
+}
